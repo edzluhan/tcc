2bd85437872faec14930c7708ccc673bd99937d8
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2018-06-21 23:20:12 -0300

    add env variable to port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = 80;
+const port = process.env.PORT || 8080;
 
 const router = express.Router();
 
