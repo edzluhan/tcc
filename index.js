56fb4a1ace893c3ef512f8295b165a48209d216a
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2018-10-29 23:57:52 -0300

    add getLists

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const insertSingleNfe = require('./lib/insertSingleNfe');
-const findProductsByDescription = require('./lib/findProduct').findProductsByDescription;
+const findProductsByDescription = require('./lib/findProduct')
+  .findProductsByDescription;
 const findProductById = require('./lib/findProduct').findProductById;
 const authUser = require('./lib/user').authUser;
 const registerUser = require('./lib/user').registerUser;
+const getLists = require('./lib/user').getLists;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,79 +22,129 @@ const port = process.env.PORT || 8080;
 
 const router = express.Router();
 
-router.post('/insert', asyncHandler(async (req, res) => {
-	const result = await insertSingleNfe(req.body.url);
-	if (result) {
-		res.statusCode = 200;
-		res.send({ "success": result });
-	} else {
-		res.statusCode = 400;
-		res.send('Bad Request');
-	}
-}));
+router.post(
+  '/insert',
+  asyncHandler(async (req, res) => {
+    const result = await insertSingleNfe(req.body.url);
+    if (result) {
+      res.statusCode = 200;
+      res.send({ success: result });
+    } else {
+      res.statusCode = 400;
+      res.send('Bad Request');
+    }
+  })
+);
 
-router.get('/findProductsByDescription', asyncHandler(async (req, res) => {
-	const result = await findProductsByDescription(req.query.description);
-	if (result) {
-		res.statusCode = 200;
-		res.send({ "success": true, result: result });
-	} else {
-		console.log('no result');
-	}
-}));
+router.get(
+  '/findProductsByDescription',
+  asyncHandler(async (req, res) => {
+    const result = await findProductsByDescription(req.query.description);
+    if (result) {
+      res.statusCode = 200;
+      res.send({ success: true, result: result });
+    } else {
+      console.log('no result');
+    }
+  })
+);
 
-router.get('/findProductById', asyncHandler(async (req, res) => {
-	const result = await findProductById(req.query.id);
-	if (result) {
-		res.statusCode = 200;
-		res.send({ "success": true, result: result });
-	} else {
-		console.log('no result');
-	}
-}));
+router.get(
+  '/findProductById',
+  asyncHandler(async (req, res) => {
+    const result = await findProductById(req.query.id);
+    if (result) {
+      res.statusCode = 200;
+      res.send({ success: true, result: result });
+    } else {
+      console.log('no result');
+    }
+  })
+);
 
-router.post('/register', asyncHandler(async (req, res) => {
-	const { name, email, password } = req.body;
-	const register = await registerUser({ name, email, password });
-	// console.log('ok', register.result.result.ok, typeof register.result.result.ok)
-	if (register.result.result.ok === 1) {
-		return res.status(200).send({ success: true });
-	}	
-}));
+router.post(
+  '/register',
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const register = await registerUser({ name, email, password });
+    // console.log('ok', register.result.result.ok, typeof register.result.result.ok)
+    if (register.result.result.ok === 1) {
+      return res.status(200).send({ success: true });
+    }
+  })
+);
 
-router.post('/login', asyncHandler(async (req, res) => {
-	const { email, password } = req.body;
-	console.log('body', req.body);
+router.post(
+  '/login',
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    console.log('body', req.body);
 
-	const user = await authUser({ email, password });
+    const user = await authUser({ email, password });
 
-	if (user) {
-		if (await bcrypt.compare(password, user.password)) {
-			const userData = { name: user.name, email: user.email };
-			const token = jwt.sign(userData, 'secret');
+    if (user) {
+      if (await bcrypt.compare(password, user.password)) {
+        const userData = { name: user.name, email: user.email };
+        const token = jwt.sign(userData, 'secret');
 
-			return res.status(200).send({ success: true, user: userData, token: token });
-		}
-	}
-	return res.status(401).send({ success: false, message: 'access denied' });
+        return res
+          .status(200)
+          .send({ success: true, user: userData, token: token });
+      }
+    }
+    return res.status(401).send({ success: false, message: 'access denied' });
+  })
+);
 
-}));
+router.get(
+  '/me',
+  asyncHandler(async (req, res) => {
+    console.log(req.headers);
+    const token = req.headers['x-access-token'];
 
-router.get('/me', asyncHandler(async (req, res) => {
-	console.log(req.headers);
-	const token = req.headers['x-access-token'];
+    if (!token) {
+      return res
+        .status(401)
+        .send({ success: false, message: 'No token provided.' });
+    }
 
-	if (!token) {
-		return res.status(401).send({ success: false, message: 'No token provided.' });
-	}
+    jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) {
+        return res
+          .status(500)
+          .send({ success: false, message: 'Failed to authenticate token.' });
+      }
+      return res.status(200).send(decoded);
+    });
+  })
+);
 
-	jwt.verify(token, 'secret', (err, decoded) => {
-		if (err) {
-			return res.status(500).send({ success: false, message: 'Failed to authenticate token.' });
-		}
-		return res.status(200).send(decoded);
-	});
-}));
+router.get(
+  '/lists',
+  asyncHandler(async (req, res) => {
+    console.log(req.headers);
+    const token = req.headers['x-access-token'];
+
+    if (!token) {
+      return res
+        .status(401)
+        .send({ success: false, message: 'No token provided.' });
+    }
+
+    jwt.verify(token, 'secret', async (err, decoded) => {
+      if (err) {
+        return res
+          .status(500)
+          .send({ success: false, message: 'Failed to authenticate token.' });
+      }
+      console.log('decoded jwt', decoded);
+	  const { email } = decoded;
+	  const lists = await getLists(email);
+	  console.log('lists', lists);
+      return res.status(200).send({ success: true, lists: lists });
+    });
+  })
+);
 
 app.use(router);
 
